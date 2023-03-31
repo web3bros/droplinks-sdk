30109befba499e076cd446ef842e2551a5b85ce4
stefanos <stliakis@gmail.com>
2023-03-31 15:37:03 +0300

    Upgraded the axios version & run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,52 @@
-import {DropLink, DropLinkClaim, DropLinksSearchOptions, NewDropLinkOptions} from "./types";
+import {
+  DropLink,
+  DropLinkClaim,
+  DropLinksSearchOptions,
+  NewDropLinkOptions,
+} from "./types";
 import axios from "axios";
 
 export class DropLinks {
   private static apiKey: string;
-  private static apiUrl: string = 'https://droplinks.io/api/v1';
+  private static apiUrl: string = "https://droplinks.io/api/v1";
 
   public static init(apiKey: string) {
     DropLinks.apiKey = apiKey;
     return this;
   }
 
-  private static async request(method: string, url: string, data: object | undefined = undefined, params: object | undefined = undefined): Promise<any> {
+  private static async request(
+    method: string,
+    url: string,
+    data: object | undefined = undefined,
+    params: object | undefined = undefined
+  ): Promise<any> {
     return axios({
       method,
       url: `${this.apiUrl}${url}`,
       data,
       params,
       headers: {
-        "X-API-KEY": this.apiKey
-      }
-    }).then(
-      response => response.data
-    ).catch(
-      error => {
-        console.error(`droplinks.io returned ${error.response.status}: ${error.response?.data?.error || error.response.statusText}`)
-        throw error
-      }
-    )
+        "X-API-KEY": this.apiKey,
+      },
+    })
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error(
+          `droplinks.io returned ${error.response.status}: ${
+            error.response?.data?.error || error.response.statusText
+          }`
+        );
+        throw error;
+      });
   }
 
   public static async create(options: NewDropLinkOptions): Promise<DropLink> {
-    return this.request(
-      'POST',
-      `/drop-links/create`,
-      {
-        campaign: options.campaign,
-        locked: options.locked
-      }
-    ).then(response => {
-      const dropLink = response.dropLinks[0]
+    return this.request("POST", `/drop-links/create`, {
+      campaign: options.campaign,
+      locked: options.locked,
+    }).then((response) => {
+      const dropLink = response.dropLinks[0];
 
       return {
         id: dropLink.id,
@@ -47,17 +55,14 @@ export class DropLinks {
         campaignId: dropLink.groupId,
         publicAddress: dropLink.publicKey,
         claimLink: dropLink.dropLinkURL,
-        claimed: false
-      } as DropLink
-    })
+        claimed: false,
+      } as DropLink;
+    });
   }
 
   public static async getClaims(): Promise<DropLinkClaim[]> {
-    return this.request(
-      'GET',
-      '/drop-links/claims'
-    ).then(response => {
-      const claims = response.claims
+    return this.request("GET", "/drop-links/claims").then((response) => {
+      const claims = response.claims;
 
       return claims.map((claim: any) => {
         return {
@@ -66,23 +71,25 @@ export class DropLinks {
           claimedByAccount: claim.claimedByAccount,
           dropLinkId: claim.dropLinkId,
           claimLink: claim.claimURL,
-          extraClaimerInfo: claim.extraClaimerInfo
-        } as DropLinkClaim
-      })
-    })
+          extraClaimerInfo: claim.extraClaimerInfo,
+        } as DropLinkClaim;
+      });
+    });
   }
 
-  public static async getDropLinks(options: DropLinksSearchOptions = {}): Promise<DropLink[]> {
+  public static async getDropLinks(
+    options: DropLinksSearchOptions = {}
+  ): Promise<DropLink[]> {
     return this.request(
-      'GET',
-      '/drop-links',
+      "GET",
+      "/drop-links",
       {},
       {
         groupId: options.campaignId,
-        claimed: options.claimed
+        claimed: options.claimed,
       }
-    ).then(response => {
-      const dropLinks = response.dropLinks
+    ).then((response) => {
+      const dropLinks = response.dropLinks;
 
       return dropLinks.map((dropLink: any) => {
         return {
@@ -92,13 +99,11 @@ export class DropLinks {
           campaignId: dropLink.groupId,
           publicAddress: dropLink.publicKey,
           claimLink: dropLink.dropLinkURL,
-          claimed: dropLink.claimed
-        } as DropLink
-      })
-    })
+          claimed: dropLink.claimed,
+        } as DropLink;
+      });
+    });
   }
 }
 
-export {
-  DropLink, DropLinkClaim, DropLinksSearchOptions, NewDropLinkOptions
-}
+export { DropLink, DropLinkClaim, DropLinksSearchOptions, NewDropLinkOptions };
--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,33 @@
 export interface DropLink {
-  id: number
-  claimCode: string,
-  publicAddress: string
-  locked: boolean
-  claimLink: string
-  campaignId: number | null
-  claimed: boolean
+  id: number;
+  claimCode: string;
+  publicAddress: string;
+  locked: boolean;
+  claimLink: string;
+  campaignId: number | null;
+  claimed: boolean;
 }
 
 interface ExtraClaimerInfo {
-  email: string
+  email: string;
 }
 
 export interface DropLinkClaim {
-  id: number
-  claimedAt: string
-  claimedByAccount: string
-  dropLinkId: number
-  claimLink: string
-  extraClaimerInfo?: ExtraClaimerInfo
+  id: number;
+  claimedAt: string;
+  claimedByAccount: string;
+  dropLinkId: number;
+  claimLink: string;
+  extraClaimerInfo?: ExtraClaimerInfo;
 }
 
 export interface NewDropLinkOptions {
-  campaign: string,
-  locked: false,
+  campaign: string;
+  locked: false;
 }
 
 export interface DropLinksSearchOptions {
-  campaignId?: number
-  claimed?: boolean
-  after?: number
+  campaignId?: number;
+  claimed?: boolean;
+  after?: number;
 }
